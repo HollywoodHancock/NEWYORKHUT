1b8f97e0d7f54d81ecedf170a27823ce7df2723c
HollyWoodHancock <info@joshhancock.com>
2026-07-22 00:43:13 -0400

    Deploy v53 universal navigation buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import site from './index-v52.js';
+import site from './index-v53.js';
 
-const DEPLOYMENT_MARKER = 'v52-homepage-navigation-injection-fix-2026-07-21-03';
+const DEPLOYMENT_MARKER = 'v53-universal-navigation-buttons-2026-07-22-01';
 
 export default {
   async fetch(request, env, ctx) {
@@ -10,16 +10,18 @@ export default {
     if (path === '/__deploy_probe') {
       return new Response(JSON.stringify({
         application: 'NewYorkHUT.com',
-        version: 'v52',
+        version: 'v53',
         deploymentMarker: DEPLOYMENT_MARKER,
         entrypoint: 'src/index.js',
-        target: 'src/index-v52.js'
+        target: 'src/index-v53.js',
+        navigation: 'universal-buttons-v53'
       }, null, 2), {
         headers: {
           'content-type': 'application/json; charset=utf-8',
           'cache-control': 'no-store, no-cache, must-revalidate, max-age=0',
-          'x-newyorkhut-version': 'v52',
-          'x-newyorkhut-deployment-marker': DEPLOYMENT_MARKER
+          'x-newyorkhut-version': 'v53',
+          'x-newyorkhut-deployment-marker': DEPLOYMENT_MARKER,
+          'x-newyorkhut-navigation': 'universal-buttons-v53'
         }
       });
     }
