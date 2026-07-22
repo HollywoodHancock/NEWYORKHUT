96f8606716a7f555dd99cd2b6c8d900b2cf2d63d
HollyWoodHancock <info@joshhancock.com>
2026-07-22 00:53:22 -0400

    Advance deployment marker for direct v53 navigation injection fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import site from './index-v53.js';
 
-const DEPLOYMENT_MARKER = 'v53-universal-navigation-buttons-2026-07-22-01';
+const DEPLOYMENT_MARKER = 'v53-direct-navigation-injection-fix-2026-07-22-02';
 
 export default {
   async fetch(request, env, ctx) {
@@ -14,14 +14,14 @@ export default {
         deploymentMarker: DEPLOYMENT_MARKER,
         entrypoint: 'src/index.js',
         target: 'src/index-v53.js',
-        navigation: 'universal-buttons-v53'
+        navigation: 'direct-universal-buttons-v53'
       }, null, 2), {
         headers: {
           'content-type': 'application/json; charset=utf-8',
           'cache-control': 'no-store, no-cache, must-revalidate, max-age=0',
           'x-newyorkhut-version': 'v53',
           'x-newyorkhut-deployment-marker': DEPLOYMENT_MARKER,
-          'x-newyorkhut-navigation': 'universal-buttons-v53'
+          'x-newyorkhut-navigation': 'direct-universal-buttons-v53'
         }
       });
     }
