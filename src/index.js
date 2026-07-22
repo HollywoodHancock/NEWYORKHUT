e729e00b6a06e4310bfd1f05560f9e2e9b60a40a
HollyWoodHancock <info@joshhancock.com>
2026-07-22 09:09:10 -0400

    Deploy MT-903 quarterly planning estimator v55

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import site from './index-v54.js';
+import site from './index-v55.js';
 
-const DEPLOYMENT_MARKER = 'v54-mt903-gvw-tax-estimator-2026-07-22-02';
+const DEPLOYMENT_MARKER = 'v55-mt903-quarterly-planning-2026-07-22-01';
 
 export default {
   async fetch(request, env, ctx) {
@@ -10,20 +10,21 @@ export default {
     if (path === '/__deploy_probe') {
       return new Response(JSON.stringify({
         application: 'NewYorkHUT.com',
-        version: 'v54',
+        version: 'v55',
         deploymentMarker: DEPLOYMENT_MARKER,
         entrypoint: 'src/index.js',
-        target: 'src/index-v54.js',
+        target: 'src/index-v55.js',
         navigation: 'legacy-css-override-visible-buttons-v53',
-        tool: 'mt903-gvw-tax-estimator-v54'
+        tool: 'mt903-quarterly-planning-v55',
+        retiredTool: 'trip-cost-estimator-redirected'
       }, null, 2), {
         headers: {
           'content-type': 'application/json; charset=utf-8',
           'cache-control': 'no-store, no-cache, must-revalidate, max-age=0',
-          'x-newyorkhut-version': 'v54',
+          'x-newyorkhut-version': 'v55',
           'x-newyorkhut-deployment-marker': DEPLOYMENT_MARKER,
           'x-newyorkhut-navigation': 'legacy-css-override-visible-buttons-v53',
-          'x-newyorkhut-tool': 'mt903-gvw-tax-estimator-v54'
+          'x-newyorkhut-tool': 'mt903-quarterly-planning-v55'
         }
       });
     }
