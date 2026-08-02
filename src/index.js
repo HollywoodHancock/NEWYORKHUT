f4faa069fa70d878d7597185479af850403f677a
HollyWoodHancock <info@joshhancock.com>
2026-08-02 11:36:47 -0400

    Activate v92 static sitemap response

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import site from './index-v91.js';
+import site from './index-v92.js';
 
-const DEPLOYMENT_MARKER = 'v91-repair-sitemap-failures-2026-08-02-01';
+const DEPLOYMENT_MARKER = 'v92-static-google-readable-sitemap-2026-08-02-01';
 
 export default {
   async fetch(request, env, ctx) {
@@ -10,25 +10,27 @@ export default {
     if (path === '/__deploy_probe') {
       return new Response(JSON.stringify({
         application: 'NewYorkHUT.com',
-        version: 'v91',
+        version: 'v92',
         deploymentMarker: DEPLOYMENT_MARKER,
         entrypoint: 'src/index.js',
-        target: 'src/index-v91.js',
-        feature: 'repair-sitemap-failures-v91',
-        repairs: {
-          recursiveVehicleLifecycleRoutesReplaced: 7,
-          overlongDescriptionCorrected: true,
-          homepageCanonicalAuditCorrected: true,
-          sitemapUrlCountPreserved: 77,
-          auditEndpoint: '/__seo_audit'
-        }
+        target: 'src/index-v92.js',
+        feature: 'static-google-readable-sitemap-v92',
+        sitemap: {
+          route: '/sitemap.xml',
+          delivery: 'direct static XML response',
+          urlCount: 77,
+          contentType: 'application/xml; charset=UTF-8',
+          robotsTxtDeclaration: true,
+          dynamicSelfFetchRemoved: true
+        },
+        auditEndpoint: '/__seo_audit'
       }, null, 2), {
         headers: {
           'content-type': 'application/json; charset=utf-8',
           'cache-control': 'no-store, no-cache, must-revalidate, max-age=0',
-          'x-newyorkhut-version': 'v91',
+          'x-newyorkhut-version': 'v92',
           'x-newyorkhut-deployment-marker': DEPLOYMENT_MARKER,
-          'x-newyorkhut-feature': 'repair-sitemap-failures-v91'
+          'x-newyorkhut-feature': 'static-google-readable-sitemap-v92'
         }
       });
     }
