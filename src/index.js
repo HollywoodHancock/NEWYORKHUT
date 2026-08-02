27e5006c610c3871f8797ce84468352ab2d54659
HollyWoodHancock <info@joshhancock.com>
2026-08-02 04:18:04 -0400

    Activate v65 carrier compliance center

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import site from './index-v64.js';
+import site from './index-v65.js';
 
-const DEPLOYMENT_MARKER = 'v64-hut-forms-library-2026-08-02-01';
+const DEPLOYMENT_MARKER = 'v65-carrier-compliance-center-2026-08-02-01';
 
 export default {
   async fetch(request, env, ctx) {
@@ -10,33 +10,29 @@ export default {
     if (path === '/__deploy_probe') {
       return new Response(JSON.stringify({
         application: 'NewYorkHUT.com',
-        version: 'v64',
+        version: 'v65',
         deploymentMarker: DEPLOYMENT_MARKER,
         entrypoint: 'src/index.js',
-        target: 'src/index-v64.js',
+        target: 'src/index-v65.js',
         navigation: 'stable-global-navigation',
-        feature: 'hut-forms-library-and-form-guides-v64',
-        hub: '/forms-library',
+        feature: 'carrier-compliance-center-and-guides-v65',
+        hub: '/carrier-compliance-center',
         guides: [
-          '/forms/tmt-1',
-          '/forms/mt-903',
-          '/forms/mt-903-i',
-          '/forms/mt-903-mn',
-          '/forms/mt-370-1',
-          '/forms/mt-370-2',
-          '/forms/tmt-39',
-          '/forms/tmt-334',
-          '/forms/dtf-406',
-          '/forms/mt-903-fut',
-          '/forms/hut-publications'
+          '/learn/ifta-compliance-for-new-york-carriers',
+          '/learn/irp-apportioned-registration',
+          '/learn/form-2290-heavy-vehicle-use-tax',
+          '/learn/unified-carrier-registration-ucr',
+          '/learn/fmcsa-registration-and-usdot-compliance',
+          '/learn/commercial-vehicle-registration-checklist',
+          '/learn/trucking-compliance-calendar'
         ]
       }, null, 2), {
         headers: {
           'content-type': 'application/json; charset=utf-8',
           'cache-control': 'no-store, no-cache, must-revalidate, max-age=0',
-          'x-newyorkhut-version': 'v64',
+          'x-newyorkhut-version': 'v65',
           'x-newyorkhut-deployment-marker': DEPLOYMENT_MARKER,
-          'x-newyorkhut-feature': 'hut-forms-library-v64'
+          'x-newyorkhut-feature': 'carrier-compliance-center-v65'
         }
       });
     }
