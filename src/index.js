5ddda2440ebcc8fb16c8716a932a1654237efa23
HollyWoodHancock <info@joshhancock.com>
2026-08-02 05:07:41 -0400

    Activate v70 worker-served header banner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import site from './index-v69.js';
+import site from './index-v70.js';
 
-const DEPLOYMENT_MARKER = 'v69-global-header-banner-2026-08-02-01';
+const DEPLOYMENT_MARKER = 'v70-worker-served-header-banner-2026-08-02-01';
 
 export default {
   async fetch(request, env, ctx) {
@@ -10,15 +10,16 @@ export default {
     if (path === '/__deploy_probe') {
       return new Response(JSON.stringify({
         application: 'NewYorkHUT.com',
-        version: 'v69',
+        version: 'v70',
         deploymentMarker: DEPLOYMENT_MARKER,
         entrypoint: 'src/index.js',
-        target: 'src/index-v69.js',
+        target: 'src/index-v70.js',
         navigation: 'stable-global-navigation',
-        feature: 'global-header-banner-v69',
+        feature: 'worker-served-header-banner-v70',
         banner: {
           asset: '/newyorkhut-header-banner.svg',
           source: 'public/newyorkhut-header-banner.svg',
+          delivery: 'worker-proxied-repository-asset',
           dimensions: '1600x300',
           placement: 'below-global-navigation'
         }
@@ -26,9 +27,9 @@ export default {
         headers: {
           'content-type': 'application/json; charset=utf-8',
           'cache-control': 'no-store, no-cache, must-revalidate, max-age=0',
-          'x-newyorkhut-version': 'v69',
+          'x-newyorkhut-version': 'v70',
           'x-newyorkhut-deployment-marker': DEPLOYMENT_MARKER,
-          'x-newyorkhut-feature': 'global-header-banner-v69'
+          'x-newyorkhut-feature': 'worker-served-header-banner-v70'
         }
       });
     }
