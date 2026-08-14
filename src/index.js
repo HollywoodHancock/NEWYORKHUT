e18dabaa8ccc6bf31c3470cd53899eeb5034eb92
HollyWoodHancock <info@joshhancock.com>
2026-08-14 11:22:44 -0500

    Deploy v94 authority SEO boundary

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import site from './index-v93.js';
+import site from './index-v94.js';
 
-const DEPLOYMENT_MARKER = 'v93-remove-sitemap-noindex-header-2026-08-02-01';
+const DEPLOYMENT_MARKER = 'v94-authority-intent-and-nyhut-referral-tracking-2026-08-14-01';
 
 export default {
   async fetch(request, env, ctx) {
@@ -10,25 +10,32 @@ export default {
     if (path === '/__deploy_probe') {
       return new Response(JSON.stringify({
         application: 'NewYorkHUT.com',
-        version: 'v93',
+        version: 'v94',
         deploymentMarker: DEPLOYMENT_MARKER,
         entrypoint: 'src/index.js',
-        target: 'src/index-v93.js',
-        feature: 'remove-sitemap-noindex-header-v93',
+        target: 'src/index-v94.js',
+        feature: 'authority-intent-and-nyhut-referral-tracking-v94',
+        seo: {
+          role: 'informational authority and education',
+          transactionDomain: 'https://www.nyhut.com/',
+          referralTracking: true,
+          utmSource: 'newyorkhut.com',
+          utmMedium: 'referral',
+          utmCampaign: 'authority_site'
+        },
         sitemap: {
           route: '/sitemap.xml',
           urlCount: 77,
           contentType: 'application/xml; charset=UTF-8',
-          xRobotsTagRemoved: true,
-          cacheControl: 'no-store during verification'
+          xRobotsTagRemoved: true
         }
       }, null, 2), {
         headers: {
           'content-type': 'application/json; charset=utf-8',
           'cache-control': 'no-store, no-cache, must-revalidate, max-age=0',
-          'x-newyorkhut-version': 'v93',
+          'x-newyorkhut-version': 'v94',
           'x-newyorkhut-deployment-marker': DEPLOYMENT_MARKER,
-          'x-newyorkhut-feature': 'remove-sitemap-noindex-header-v93'
+          'x-newyorkhut-feature': 'authority-intent-and-nyhut-referral-tracking-v94'
         }
       });
     }
